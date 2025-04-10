255d0a9380be679e30559792ca6dda8cc32c4588
Oleksandra Kibko <kibko.sasha@gmail.com>
2025-04-10 18:48:24 +0300

    Constraint Layout playground with decoupling

--- app/src/main/java/com/example/helloandroidagain/ui/basic/screen/ConstraintScreen.kt
+++ app/src/main/java/com/example/helloandroidagain/ui/basic/screen/ConstraintScreen.kt
@@ -12,74 +12,40 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
 import com.example.helloandroidagain.R
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun ConstraintScreen(modifier: Modifier = Modifier) {
-    ConstraintLayout(modifier = modifier.fillMaxSize()) {
-        val topGuideline = createGuidelineFromTop(0.18f)
-        val bottomGuideline = createGuidelineFromBottom(0.2f)
-        val (
-            mainImage,
-            inputName,
-            inputRow,
-            buttonRow
-        ) = createRefs()
-
-        val chain = createVerticalChain(
-            mainImage,
-            inputName,
-            inputRow,
-            buttonRow,
-            chainStyle = ChainStyle.SpreadInside
-        )
-        constrain(chain) {
-            top.linkTo(topGuideline)
-            bottom.linkTo(bottomGuideline)
-        }
-
+    ConstraintLayout(
+        modifier = modifier.fillMaxSize(),
+        constraintSet = constraints()
+    ) {
         Image(
             painter = painterResource(R.drawable.baseline_camera),
             contentDescription = "Test Description",
             modifier = Modifier
-                .constrainAs(mainImage) {
-                    top.linkTo(topGuideline)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                    bottom.linkTo(inputName.top)
-                    width = Dimension.percent(0.9f)
-                    height = Dimension.ratio("4:3")
-                }
+                .layoutId("mainImage")
                 .background(Color.Gray)
         )
 
         OutlinedTextField(
             value = "Name",
             onValueChange = {},
-            modifier = Modifier.constrainAs(inputName) {
-                top.linkTo(mainImage.bottom, margin = 16.dp)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-                bottom.linkTo(inputRow.top)
-                width = Dimension.percent(0.8f)
-            }
+            modifier = Modifier.layoutId("inputName")
         )
 
         Row(
             modifier = Modifier
-                .constrainAs(inputRow) {
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                    top.linkTo(inputName.bottom)
-                    bottom.linkTo(buttonRow.top)
-                }
+                .layoutId("inputRow")
                 .fillMaxWidth(0.8f),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
@@ -89,12 +55,7 @@ fun ConstraintScreen(modifier: Modifier = Modifier) {
 
         Row(
             modifier = Modifier
-                .constrainAs(buttonRow) {
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                    top.linkTo(inputRow.bottom)
-                    bottom.linkTo(bottomGuideline)
-                }
+                .layoutId("buttonRow")
                 .fillMaxWidth(0.8f),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
@@ -105,5 +66,53 @@ fun ConstraintScreen(modifier: Modifier = Modifier) {
                 Text("Do Again")
             }
         }
+    }
+}
+
+private fun constraints() = ConstraintSet {
+    val topGuideline = createGuidelineFromTop(0.18f)
+    val bottomGuideline = createGuidelineFromBottom(0.2f)
+    val mainImage = createRefFor("mainImage")
+    val inputName = createRefFor("inputName")
+    val inputRow = createRefFor("inputRow")
+    val buttonRow = createRefFor("buttonRow")
+    val chain = createVerticalChain(
+        mainImage,
+        inputName,
+        inputRow,
+        buttonRow,
+        chainStyle = ChainStyle.SpreadInside
+    )
+
+    constrain(chain) {
+        top.linkTo(topGuideline)
+        bottom.linkTo(bottomGuideline)
+    }
+    constrain(mainImage) {
+        top.linkTo(topGuideline)
+        start.linkTo(parent.start)
+        end.linkTo(parent.end)
+        bottom.linkTo(inputName.top)
+        width = Dimension.percent(0.9f)
+        height = Dimension.ratio("4:3")
+    }
+    constrain(inputName) {
+        top.linkTo(mainImage.bottom, margin = 16.dp)
+        start.linkTo(parent.start)
+        end.linkTo(parent.end)
+        bottom.linkTo(inputRow.top)
+        width = Dimension.percent(0.8f)
+    }
+    constrain(inputRow) {
+        start.linkTo(parent.start)
+        end.linkTo(parent.end)
+        top.linkTo(inputName.bottom)
+        bottom.linkTo(buttonRow.top)
+    }
+    constrain(buttonRow) {
+        start.linkTo(parent.start)
+        end.linkTo(parent.end)
+        top.linkTo(inputRow.bottom)
+        bottom.linkTo(bottomGuideline)
     }
 }
